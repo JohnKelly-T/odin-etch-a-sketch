7e6b500a7dc4c1d4b422dc91cfb7e8fae6917c1e
john-kelly <johnkellytemena@gmail.com>
2024-11-03 19:24:01 +0800

    feat: Make clear function

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function createGrid(dimension) {
 let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
+document.body.onmouseleave = () => (mouseDown = false);
 
 function draw(element) {
     element.style.backgroundColor = "#646464";
@@ -25,6 +26,15 @@ function draw(element) {
 grid.addEventListener("mouseover", (e) => {
     if (mouseDown === true) {
         draw(e.target);
+    }
+})
+
+let clearButton = document.querySelector("#clear-btn");
+
+clearButton.addEventListener("click", () => {
+    let gridChildren = grid.children;
+    for (let i = 0; i < gridChildren.length; i++) {
+        gridChildren[i].style.backgroundColor = "#D9D9D9";
     }
 })
 
