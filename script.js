75fb053c12866072115981a4df7059d60c1489c2
john-kelly <johnkellytemena@gmail.com>
2024-11-03 22:17:51 +0800

    feat: Add darkening functionality

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+const DEFAULT_COLOR = "#646464";
+
 let grid = document.querySelector("#grid");
 let gridWidth = grid.getBoundingClientRect().width;
 let clearButton = document.querySelector("#clear-btn");
 let gridSizeSlider = document.querySelector("#grid-size-slider");
 let gridSizeText = document.querySelector(".grid-size-text");
 let colorPicker = document.querySelector("#color-picker");
+let darkeningButton = document.querySelector("#darkening-btn");
 
 let currentColor = "#646464";
+let darkening = false;
 
 let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true);
@@ -22,7 +26,8 @@ function createGrid(dimension) {
         let gridSquare = document.createElement("div");
         gridSquare.style.width = (gridWidth / dimension) + "px";
         gridSquare.style.height = (gridWidth / dimension) + "px";
-        
+        gridSquare.style.opacity = 0;
+
         gridSquare.addEventListener("mousedown", (e) => {
             colorSquare(e.target);
         })
@@ -37,8 +42,15 @@ function createGrid(dimension) {
     }
 }
 
-function colorSquare(element) {
+function colorSquare(element) { 
     element.style.backgroundColor = currentColor;
+
+    if (darkening === false) {
+        element.style.opacity = 1;
+    } else {
+        element.style.opacity = parseFloat(element.style.opacity) + .10;
+    }
+  
 }
 
 function updateGridSize(value) {
@@ -58,7 +70,11 @@ clearButton.addEventListener("click", () => {
     let gridChildren = grid.children;
     for (let i = 0; i < gridChildren.length; i++) {
         gridChildren[i].style.backgroundColor = "#D9D9D9";
+        gridChildren[i].style.opacity = 0;
     }
 })
 
+darkeningButton.addEventListener("click", () => {
+    darkening = (darkening === true) ? false : true;
+})
 createGrid(24);
